86da3ea411f10b93f46c44d701ca4bd60b672cb7
Taha Baba <taha9906@gmail.com>
2026-09-22 22:07:03 +0530

    Implementing Image Editor using Facade pattern and adding main

--- ImageEditing_FacadePattern/src/Main.java
+++ ImageEditing_FacadePattern/src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // --------------------------------------------------
+        // 1. Create the subsystem services
+        // --------------------------------------------------
+
+        ImageLoader imageLoader =
+                new ImageLoader();
+
+        FilterService filterService =
+                new FilterService();
+
+        ImageModifier imageModifier =
+                new ImageModifier();
+
+        ImageWriter imageWriter =
+                new ImageWriter();
+
+        AnalyticService analyticsService =
+                new AnalyticService();
+
+
+        // --------------------------------------------------
+        // 2. Create the ImageEditingManager
+        // --------------------------------------------------
+
+        ImageEditingManager manager =
+                new ImageEditingManager(
+                        imageLoader,
+                        filterService,
+                        imageModifier,
+                        imageWriter,
+                        analyticsService
+                );
+
+
+        // --------------------------------------------------
+        // 3. Ask the manager to edit an image
+        // --------------------------------------------------
+
+        manager.editImage(
+                "vacation.jpg",
+                "GRAYSCALE",
+                25
+        );
+
     }
 }
